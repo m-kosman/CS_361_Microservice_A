88f65c40c3c0def542445fdbdd8c8848e4710117
m.kosman <mary.kosman@gmail.com>
2024-11-13 18:25:52 +0900

    fixed server issues wher it was not sending the tasks to the workers.

--- zeromq_server.py
+++ zeromq_server.py
@@ -1,70 +1,177 @@
 import time
+from datetime import datetime
 import zmq
 import json
-
-
+import signal
+import sys
 
 class CategoryServer:
     def __init__(self, host="localhost", port=8888):
         self._context = zmq.Context()
         # for connection with client program
-        self._socket = self._context.socket(zmq.REP)
-        self._socket.bind(f"tcp://{host}:{port}")
+        # self._socket = self._context.socket(zmq.REP)
+        # self._socket.bind(f"tcp://{host}:{port}")
+        self._frontend = self._context.socket(zmq.ROUTER)
+        self._frontend.bind(f"tcp://{host}:{port}")
 
         # for communicating with workers
-        self._push_socket = self._context.socket(zmq.PUSH)
-        self._push_socket.bind(f"tcp://{host}:{port+1}")  # 8889
+        # self._rep_socket = self._context.socket(zmq.PUSH)
+        # self._rep_socket.bind(f"tcp://{host}:{port+1}")  # 8889
+        self._backend = self._context.socket(zmq.DEALER)
+        self._backend.bind(f"tcp://{host}:{port + 1}")  # 8889
 
-        self._pull_socket = self._context.socket(zmq.PULL)
-        self._pull_socket.bind(f"tcp://{host}:{port+2}")  # 8890
+        # self._rep_socket = self._context.socket(zmq.REP)
+        # self._rep_socket.bind(f"tcp://{host}:{port+2}")  # 8890
+        # self._pull_socket = self._context.socket(zmq.PULL)
+        # self._pull_socket.bind(f"tcp://{host}:{port+2}")  # 8890
+        signal.signal(signal.SIGINT, self.close)
+        signal.signal(signal.SIGTERM, self.close)
+
+        self._poller = zmq.Poller()
+        self._poller.register(self._frontend, zmq.POLLIN)
+        self._poller.register(self._backend, zmq.POLLIN)
 
     def process_requests(self):
         while True:
-            try:
-                # receives request from client program
-                message = self._socket.recv_json()
-                print(f"Received request: {message}")
+            sockets = dict(self._poller.poll())
 
-                if message["message type"] == "request":
-                    # send tasks to be categorized
-                    response = {
-                        "message type": "response",
-                        "tasks": []
-                    }
-                    # sends tasks to workers
-                    for task in message["tasks"]:
-                        self._push_socket.send_json(task)
+            if sockets.get(self._frontend) == zmq.POLLIN:
+                message = self._frontend.recv_multipart()
+                print(f"sending message: {message}")
+                client_id, message, request = self.partition_message(message)
+                num_tasks = len(message["tasks"])
+                if request:
+                    self.distribute_tasks(client_id, message)
+                    response = self.get_responses(num_tasks)
+                    print(response)
+                    multipart_msg = [client_id, b'', json.dumps(response).encode()]
+                    print(f"sending response: {multipart_msg} ")
+                    self._frontend.send_multipart(multipart_msg)
+                else:
+                    self.process_feedback()
 
-                    # collects responses from workers
-                    for _ in message["tasks"]:
-                        new_task = self._pull_socket.recv_json()
-                        response["tasks"].append(new_task)
-                    pass
 
-                if message["message type"] == "feedback":
-                    # process feedback
-                    task = message['feedback']['task']
-                    orig_category = message['feedback']['category_provided']
-                    new_category = message['feedback']['category_feedback']
+            # try:
+            #     # receives request from client program
+            #     multipart_message = self._frontend.recv_multipart()
+            #     print(multipart_message)
+            #     message = multipart_message[2]
+            #     client_id = multipart_message[0]   # 1 is empty byte string
+            #     # message = self._socket.recv_json()
+            #
+            #     try:
+            #         message_str = message.decode("utf-8")
+            #         print(f"Received request: {message}")
+            #         message_dict = json.loads(message)
+            #     except json.JSONDecodeError as e:
+            #         print(f"Error decoding JSON: {e}")
+            #         continue
+            #     try:
+            #         if request:
+                        # send tasks to be categorized
+                        # response = {
+                        #     "message type": "response",
+                        #     "tasks": []
+                        # }
+                        # num_tasks = len(message["tasks"])
+                        # # sends tasks to workers
+                        # for task in message["tasks"]:
+                        #     try:
+                        #         print(f"Sending: {task}")
+                        #         serialized_task = json.dumps(task).encode('utf-8')
+                        #         multipart_message = [client_id, serialized_task]
+                        #         print(f"message to send: {multipart_message}")
+                        #         self._backend.send_multipart(multipart_message)  # {task_id: , task: }
+                        #     except Exception as e:
+                        #         print(f"Error sending task: {e}")
 
-                    response = {"message type": "response",
-                                "message": f"Received feedback for task: {task}.  Category should be {new_category} "
-                                           f"instead of {orig_category}."
-                                }
 
-                    self._socket.send_json(response)
-            except zmq.Again as e:
-                time.sleep(0.1)
-            except zmq.ZMQError as error:
-                print(f"Error while receiving message: {error}")
-                print(f"ZMQ Error Code: {error.errno}")
-                print(f"ZMQ Error Message: {zmq.strerror(error.errno)}")
-                break
-    def close(self):
-        self._socket.close()
-        self._push_socket.close()
-        self._pull_socket.close()
+                        # collects responses from workers
+                        # for _ in range(num_tasks+1):
+                        #     print("Collecting worker responses")
+                        #     try:
+                        #         if self._rep_socket in sockets:
+                        #             new_task = self._rep_socket.recv_json()
+                        #             print(f"Received response from worker:{new_task}")
+                        #             response["tasks"].append(new_task)
+                        #         else:
+                        #             print("No response from worker within timeout")
+                        #     except Exception as e:
+                        #         print(f"Error receiving task: {e}")
+
+                        # print(f"Sending: {response}")
+                        # self._frontend.send_multipart([client_id, json.dumps(response).encode("utf-8")])
+
+                #     else:
+                #         # process feedback
+                #         task = message['feedback']['task']
+                #         orig_category = message['feedback']['category_provided']
+                #         new_category = message['feedback']['category_feedback']
+                #
+                #         response = {"message type": "response",
+                #                     "message": f"Received feedback for task: {task}.  Category should be {new_category} "
+                #                                f"instead of {orig_category}."
+                #                     }
+                #
+                #         self._frontend.send_json(response)
+                # except zmq.Again as e:
+                #     time.sleep(0.1)
+                # except zmq.ZMQError as error:
+                #     print(f"Error while receiving message: {error}")
+                #     print(f"ZMQ Error Code: {error.errno}")
+                #     print(f"ZMQ Error Message: {zmq.strerror(error.errno)}")
+                #     break
+
+    def partition_message(self, message):
+        message_text = message[2].decode("utf-8")
+        client_id = message[0]  # 1 is empty byte string
+
+        print(f"Received request: {message}")
+        message_dict = json.loads(message_text)
+
+        request = message_dict.get("message type") == "request"
+
+        return client_id, message_dict, request
+
+    def distribute_tasks(self, client_id, message):
+        del message["message type"]  # removes the message type
+        tasks = message["tasks"]    # flattens remaining
+
+        for task in tasks:
+            json_task = json.dumps(task).encode('utf-8')
+            print(f"sending tasks to worker {datetime.now()}")
+            self._backend.send_multipart([client_id, json_task])
+
+    def get_responses(self, total):
+        sockets = dict(self._poller.poll())
+        response = {
+            "message type": "response",
+            "tasks": []
+        }
+        responses = 0
+
+        while responses < total:
+            if sockets.get(self._backend) == zmq.POLLIN:
+                message = self._backend.recv_multipart()
+                client_id = message[0]
+                new_task = json.loads(message[1])
+                print(f"Received response from worker: {new_task} {datetime.now()}")
+                response["tasks"].append(new_task)
+                responses += 1
+        print(f"all responses received {datetime.now()}")
+        return response
+
+
+    def process_feedback(self):
+        pass
+
+    def close(self, signalnum, frame):
+        self._frontend.close()
+        self._backend.close()
+        self._rep_socket.close()
         self._context.term()
+        print("Server Closed")
+
 
 if __name__ == "__main__":
     server = CategoryServer()
