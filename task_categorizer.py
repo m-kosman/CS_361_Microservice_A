7c6e26f267e70cb5c00a1d0e287770d9a2419e32
m.kosman <mary.kosman@gmail.com>
2024-11-13 12:30:18 +0900

    added functionality to set category to personal if no matches found

--- task_categorizer.py
+++ task_categorizer.py
@@ -1,8 +1,6 @@
 import string
 
-from nltk import SnowballStemmer
-
-from task_category_db import TaskCategoryDatabase
+from task_category_db import TaskCategoryDatabase, Categories
 import nltk
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
@@ -12,15 +10,24 @@ nltk.download('punkt_tab')
 nltk.download('wordnet')
 
 
-
-
 class TaskCategorization:
     def __init__(self, task_id, task):
         self._task_id = task_id
         self._task = task
+        self._categories = None
+        self._database = TaskCategoryDatabase()
 
+
+        self.get_categories()
         self._processed_task = self.preprocess_string(self._task)
         self._category = self.find_category(self._processed_task)
+
+    def get_categories(self):
+        """initalizes dictionary of categories to store # of db matches """
+        if self._categories is None:
+            self._categories = {}
+            for category in Categories:
+                self._categories[category.value] = 0
 
     def preprocess_string(self, task):
         """handles preprocessing of the task for use in db queries"""
@@ -33,14 +40,11 @@ class TaskCategorization:
     def tokenize_string(self, task):
         """separates string into individual words and removes punctuation/filler words"""
         tokens = nltk.word_tokenize(task)
-        print(tokens)
         punctuation = set(string.punctuation)
         filler_words = set(stopwords.words("english"))
 
         no_punctuation = [word for word in tokens if word not in punctuation]
-        print(no_punctuation)
         filtered_tokens = [word for word in no_punctuation if word not in filler_words]
-        print(filtered_tokens)
 
         return filtered_tokens
 
@@ -48,12 +52,51 @@ class TaskCategorization:
         """stems words using a lemmatizer"""
         lemmatizer = WordNetLemmatizer()
         lemmed_tokens = [lemmatizer.lemmatize(word) for word in tokens]
-        print(lemmed_tokens)
         return lemmed_tokens
 
     def find_category(self, processed_tokens):
-        pass
+        for key in self._categories:
+            # gets the keywords
+            cat_id = self._database.get_category_id(key)
+            keywords = self._database.get_all_keywords_for_category(cat_id)
 
+            # preprocesses the keywords
+            processed_keywords = []
+            for keyword in keywords:
+                result = self.preprocess_string(keyword)
+                processed_keywords.extend(result)
+
+            # checks for any matches
+            matches = 0
+            for keyword in processed_keywords:
+                if keyword in processed_tokens:
+                    matches += 1
+
+            self._categories[key] = matches
+        # gets key with max value
+        category = max(self._categories, key=self._categories.get)
+
+        # if the key has a 0 value then sets it to the default category personal
+        if self._categories[category] == 0:
+            category = "personal"
+
+        print(self._categories)
+        print(category)
+        return category
+
+
+task1 = 'Complete homework assignment'
+task2 = 'Prepare presentation slides'
+task3 = 'Grocery shopping for the week'
+task4 = 'Attend team meeting at 3 PM'
+task5 = 'Write blog post for the website'
+task6 = 'Call the doctor for an appointment'
+task7 = 'Clean and organize the workspace'
+task8 = 'Respond to work emails'
+task9 = 'Pick up dry cleaning'
+task10 = 'Walk the dog in the evening'
+
+categorizer = TaskCategorization(1, task7)
 
 
 
