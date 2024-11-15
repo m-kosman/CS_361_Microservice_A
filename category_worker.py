ff5ff8467f3e2ab0b99efe8ca40d94557fcc0607
m.kosman <mary.kosman@gmail.com>
2024-11-15 16:13:10 +0900

    added docstrings, comments and removed old commented code

--- category_worker.py
+++ category_worker.py
@@ -1,5 +1,3 @@
-import sys
-import time
 import zmq
 import json
 from task_categorizer import TaskCategorization
@@ -8,13 +6,10 @@ from datetime import datetime
 
 
 class CategoryWorker:
+    """Represents a worker who categorizes tasks"""
     def __init__(self, host="localhost", port=8889):
         self._context = zmq.Context()
-        # self._pull_socket = self._context.socket(zmq.PULL)
-        # self._pull_socket.connect(f"tcp:{host}:{port}")
-        #
-        # self._rep_socket = self._context.socket(zmq.PUSH)
-        # self._rep_socket.connect(f"tcp:{host}:{port+1}")  # 8890
+
         self._deal_socket = self._context.socket(zmq.DEALER)
         self._deal_socket.connect(f"tcp://{host}:{port}")
         print(f"Worker created and connected to dealersocket {self._deal_socket}")
@@ -22,51 +17,47 @@ class CategoryWorker:
         self._poller = zmq.Poller()
         self._poller.register(self._deal_socket, zmq.POLLIN)
 
-        # self._rep_socket = self._context.socket(zmq.REP)
-        # self._rep_socket.connect(f"tcp://{host}:{port + 1}")  # 8890
+        # for handling socket close
         signal.signal(signal.SIGINT, self.close)
         signal.signal(signal.SIGTERM, self.close)
 
     def process_tasks(self):
+        """
+        Handles the main event loop for listening for tasks calling the task categorizer and
+        returning a category.
+        """
         while True:
             sockets = dict(self._poller.poll())
 
+            # checks for a message, processes it and returns the category
             if sockets.get(self._deal_socket) == zmq.POLLIN:
+                # receives and unpacks the message
                 message = self._deal_socket.recv_multipart()  # {task_id:, task:}
                 print(f"Worker {self} received message: {message} {datetime.now()}")
                 client_id = message[0]
                 tasks = message[1]
                 task = json.loads(tasks.decode('utf-8'))
 
+                # gets the response to send
                 response = self.get_category(task)
 
+                # sends message to main server
                 print(f"about to send:{response} {datetime.now()}")
                 json_response = json.dumps(response).encode()
-                # sends message to main server
                 self._deal_socket.send_multipart([client_id, json_response])
-                # print(f"received message: {task}")
-                #
-                # # add categorization
-                # categorizer = TaskCategorization(task["task_id"], task["task"])
-                # category = categorizer.get_category()
-                #
-                # # creates message
-                # result = {
-                #     "task_id": task['task_id'],
-                #     "task": task['task'],
-                #     "category": category
-                # }
-                # print(f"about to send:{result}")
-                #
-                # # sends message to main server
-                # self._rep_socket.send_json(result)
 
-    def get_category(self, task):
-        # add categorization
+    def get_category(self, task:dict) -> dict:
+        """
+        Calls the task categorizer and creates the response message to
+        send to server.
+        :param task: a dictionary of the task w/ id and task
+        :return: A dictionary with the task and category
+        """
+        # gets the categorization
         categorizer = TaskCategorization(task["task_id"], task["task"])
         category = categorizer.get_category()
 
-        # creates message
+        # creates message w/ new c ategory
         result = {
             "task_id": task['task_id'],
             "task": task['task'],
@@ -74,9 +65,8 @@ class CategoryWorker:
         }
         return result
 
-
     def close(self, signalnum, frame):
-        # self._rep_socket.close()
+        """Handles closing a socket"""
         self._deal_socket.close()
         self._context.term()
         print("Workers closed")
